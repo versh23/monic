d7f3516e80fa4aebe97cb3eb27e84aa6bdbb3b80
Sergey <versh23@gmail.com>
2013-02-05 15:19:08 +0400

    Update _monic/monic_small.php

--- _monic/monic_small.php
+++ _monic/monic_small.php
@@ -8,14 +8,18 @@ $url='/_monic/monic_small.php?server='.$server.'&table='.$table;
 $pq = PQ::create($conf);
 if($pq->query_ping($server))
 {
-    $info_server = $pq->query_info($server);
+	$hltv = false;
+	if($table == 'monic7'){
+		$hltv = $pq->query_ping('iphltv');
+	}
+        $info_server = $pq->query_info($server);
 	$link=mysql_connect($host,$login,$pass) or die('No connect to database');
 	mysql_select_db('web913612_ps') or die('no connetc to database');
 	$res=mysql_query("SELECT * FROM $table ORDER BY `frag` DESC");
 	if(!$res) die("cant select data in $table");
-	$cnt=mysql_num_rows($res)-1;//последний строка номер
-	$map=mysql_result($res,$cnt,5);//название карты
-	mysql_data_seek($res,0);//переводв начало
+	$cnt=mysql_num_rows($res)-1;//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+	$map=mysql_result($res,$cnt,5);//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+	mysql_data_seek($res,0);//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 	mysql_close();
 	include('monic_min_on.tpl');	
 }else{
